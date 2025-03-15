64cc5ed9ddfc2be4f4d9a8f965388a801c792bd8
jmveigac <jmveigac@gmail.com>
2025-03-15 16:54:14 +0100

    first commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using rest_api_base.Models;
+using rest_api_base.Services;
+
+namespace rest_api_base.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController() { }
+
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+        return pizza == null ? NotFound() : pizza;
+    }
+
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id) return BadRequest();
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null) return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null) return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,8 @@
+namespace rest_api_base.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+}
