019f063938c2b1f46a33892fecf03327353c52f7
Jose Manuel Veiga Castillo <44842389+jmveigac@users.noreply.github.com>
2026-07-20 20:45:23 +0200

    Use xUnit global test imports

--- tests/rest-api-base.Tests/ApiTests.cs
+++ tests/rest-api-base.Tests/ApiTests.cs
@@ -3,7 +3,6 @@ using System.Net.Http.Json;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using rest_api_base.Models;
-using Xunit;
 
 namespace rest_api_base.Tests;
 
