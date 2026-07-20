a1d32367bb15996c3abe587855bb1c61443c4240
Jose Manuel Veiga Castillo <44842389+jmveigac@users.noreply.github.com>
2026-07-20 20:36:30 +0200

    Configure health checks and injected services

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
+using rest_api_base.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+builder.Services.AddSingleton<IPizzaService, PizzaService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,8 +18,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
+app.MapHealthChecks("/health");
 app.MapControllers();
 
 app.Run();
+
+public partial class Program;
